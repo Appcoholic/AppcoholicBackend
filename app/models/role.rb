3ae3c4540a22552bd85b14c6cfde38fc258601d7
Jaime Alvarez <jaime_andres@unitec.edu>
2016-04-28 03:18:37 +0000

    :optional Fix in Role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,9 +1,11 @@
 class Role < ActiveRecord::Base
   has_and_belongs_to_many :users, :join_table => :users_roles
-
+  
+  
+  # :optional Error Fix Reference: 
+  # http://stackoverflow.com/questions/36144877/rolify-table-error-user-add-role-admin-unknown-key-error/36151534#36151534
   belongs_to :resource,
-             :polymorphic => true,
-             :optional => true
+             :polymorphic => true
 
   validates :resource_type,
             :inclusion => { :in => Rolify.resource_types },

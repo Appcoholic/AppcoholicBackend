ab7c62e9a312a54957d1c24d9d9ba51bbe6317aa
Jaime Alvarez <jaime_andres@unitec.edu>
2016-05-25 03:26:47 +0000

    Add CanCan access denied rescue error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
   
-  def after_sign_in_path_for(resource)
-    dashboard_path
-  end
+    # If CanCan privileges are denied
+    rescue_from CanCan::AccessDenied do |e|
+        flash[:danger] = "You do not have permission to perform this action."
+        redirect_to dashboard_path
+    end
+  
+    def after_sign_in_path_for(resource)
+        dashboard_path
+    end
 end

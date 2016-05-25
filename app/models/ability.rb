0985178a7b13b1dc4b141871d1a5360e3bdea937
Jaime Alvarez <jaime_andres@unitec.edu>
2016-05-25 03:32:52 +0000

    Add guest user to Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,12 +2,16 @@ class Ability
     include CanCan::Ability
 
     def initialize(user)
+        user ||= User.new # guest user (not logged in)
         if user.has_role? :admin
             can :manage, :all
             
         elsif user.has_role? :courier
             can :read, Product
             can :read, Order
+        else
+            can :create, Order
+            can :track, Order
         end
     end
     
